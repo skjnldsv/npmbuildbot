a92b16f0c76888c24f0a0ec188799519efb89e75
Glitch (skjnldsv-npmbuildbot) <none>
2019-06-09 19:26:12 +0000

    🏞🚛 Updated with Glitch

--- index.js
+++ index.js
@@ -8,8 +8,9 @@ const commands = require('probot-commands')
 module.exports = app => {
 	const logger = app.log
 
-	const deny = (repo, number) =>
+	const deny = (repo, number) => {
 		logger.info(`Invalid request: ${repo}#${number}`)
+  }
 
 	commands(app, 'compile', async (context, command) => {
 		
@@ -17,22 +18,34 @@ module.exports = app => {
 		const repo = payload.repository.full_name
 		const number = payload.issue.number
 
-		// 1. checking if this is a PR
+		// PR checks
 		if (!payload.issue.html_url.endsWith('pull/' + payload.issue.number)) {
 			// Ignore normal issues
 			logger.debug('This is not a PR')
 			deny(repo, number)
+		  comment.minusOne(context, payload.comment.id)
+			return
+		}
+    const isMerged = await pr.isMerged(context, payload.issue.number)
+    const isClosed = await pr.isClosed(context, payload.issue.number)
+		if (isMerged || isClosed) {
+      // should not be merged
+			logger.debug('PR is already merged or closed just carry on')
+			deny(repo, number)
+		  comment.minusOne(context, payload.comment.id)
 			return
 		}
 
 		// 2. command syntax checks
 		if (!command.arguments.startsWith('/')) {
 			logger.debug('Path does not start with a /')
+		  comment.confused(context, payload.comment.id)
 			deny(repo, number)
 			return
 		}
 		if (command.arguments.trim().indexOf(' ') > -1) {
 			logger.debug('Path contains spaces')
+		  comment.confused(context, payload.comment.id)
 			deny(repo, number)
 			return
 		}
@@ -40,12 +53,6 @@ module.exports = app => {
 		// all clear! Go go go!
 		const path = command.arguments.trim().substr(1)
 		comment.plusOne(context, payload.comment.id)
-
-		if (await pr.isMerged(context, payload.issue.number)) {
-			logger.debug('PR is already merged just carry on')
-			deny(repo, number)
-			return
-		}
 
 		const token = await getToken(context.payload.installation.id)
 		const branch = await pr.getBranch(context)
@@ -67,9 +74,11 @@ module.exports = app => {
 
 		if (success) {
 			logger.info(`Successfully pushed commit ${success} to branch ${branch} on ${repo}#${number}`)
+		  comment.rocket(context, payload.comment.id)
 		} else {
 			logger.debug(`The provided path ${path} does not contain any changes to commit`)
 			deny(repo, number)
+		  comment.confused(context, payload.comment.id)
 		}
 	})
 }
