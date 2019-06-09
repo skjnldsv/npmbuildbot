f893a4df3f5477f13c4d7440a82dd7235b54374d
Glitch (skjnldsv-npmbuildbot) <none>
2019-06-09 18:26:18 +0000

    🚎💨 Updated with Glitch

--- index.js
+++ index.js
@@ -1,66 +1,75 @@
-require('newrelic')
+const pr = require('./lib/pr.js')
+const comment = require('./lib/comment.js')
+const git = require('./lib/git.js')
+const compile = require('./lib/compile.js')
+const getToken = require('./lib/token.js')
+const commands = require('probot-commands')
 
-const getConfig = require('probot-config')
-const createScheduler = require('probot-scheduler')
-const Stale = require('./lib/stale')
-
-module.exports = async app => {
-  // Visit all repositories to mark and sweep stale issues
-  const scheduler = createScheduler(app)
-
-  // Unmark stale issues if a user comments
-  const events = [
-    'issue_comment',
-    'issues',
-    'pull_request',
-    'pull_request_review',
-    'pull_request_review_comment'
-  ]
-
-  app.on(events, unmark)
-  app.on('schedule.repository', markAndSweep)
-
-  async function unmark (context) {
-    if (!context.isBot) {
-      const stale = await forRepository(context)
-      let issue = context.payload.issue || context.payload.pull_request
-      const type = context.payload.issue ? 'issues' : 'pulls'
-
-      // Some payloads don't include labels
-      if (!issue.labels) {
-        try {
-          issue = (await context.github.issues.get(context.issue())).data
-        } catch (error) {
-          context.log('Issue not found')
-        }
-      }
-
-      const staleLabelAdded = context.payload.action === 'labeled' &&
-        context.payload.label.name === stale.config.staleLabel
-
-      if (stale.hasStaleLabel(type, issue) && issue.state !== 'closed' && !staleLabelAdded) {
-        stale.unmarkIssue(type, issue)
-      }
+module.exports = app => {
+  const logger = app.log
+  
+  const deny = (repo, number) => logger.info(`Invalid request: ${repo}#${number}`)
+  
+  commands(app, 'compile', async (context, command) => {
+    // Checking if this is a PR
+		const payload = context.payload
+    const repo = payload.repository.full_name
+    const number = payload.issue.number
+    
+    if (!payload.issue.html_url.endsWith('pull/' + payload.issue.number)) {
+			// Ignore normal issues
+			logger.debug('This is not a PR')
+      deny(repo, number)
+			return
+		}
+    
+    // Command syntax checks    
+    if (!command.arguments.startsWith('/')) {
+      logger.debug('Path does not start with a /')
+      deny(repo, number)
+      return
     }
-  }
-
-  async function markAndSweep (context) {
-    const stale = await forRepository(context)
-    await stale.markAndSweep('pulls')
-    await stale.markAndSweep('issues')
-  }
-
-  async function forRepository (context) {
-    let config = await getConfig(context, 'stale.yml')
-
-    if (!config) {
-      scheduler.stop(context.payload.repository)
-      // Don't actually perform for repository without a config
-      config = { perform: false }
+    
+    if (command.arguments.trim().indexOf(' ') > -1) {
+      logger.debug('Path contains spaces')
+      deny(repo, number)
+      return
     }
+    
+    // all clear! Go go go!
+    const path = command.arguments.trim().substr(1)
+		comment.plusOne(context, payload.comment.id)
 
-    config = Object.assign(config, context.repo({ logger: app.log }))
+		if (await pr.isMerged(context, payload.issue.number)) {
+		  logger.debug('PR is already merged just carry on')
+      deny(repo, number)
+		  return
+		}
 
-    return new Stale(context.github, config)
-  }
+
+		const token = await getToken(context.payload.installation.id)
+		const branch = await pr.getBranch(context)
+		logger.info(`Starting branch ${branch} with path /${path} on ${repo}#${number} `)
+
+		// cloning
+		const gitRoot = await git.cloneAndCheckout(context, token, branch)
+		if (!gitRoot) {
+			logger.debug('Error during the git initialisation')
+      deny(repo, number)
+			return
+		}
+
+		// compiling app
+		await compile(gitRoot, logger)
+
+		// commit and push
+		const success = await git.commitAndPush(context, path, branch, token, gitRoot, logger)
+
+		if (success) {
+			logger.info(`Successfully pushed commit ${success} to branch ${branch} on ${repo}#${number}`)
+		} else {
+			logger.debug(`The provided path ${path} does not contain any changes to commit`)
+      deny(repo, number)
+		}
+  })
 }
